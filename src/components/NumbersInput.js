75dfbe0eadbb6b85fce95305ea948cce7774a94d
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-05-03 23:55:55 -0300

    Funcionalidade principais da calculadora

--- src/components/NumbersInput.js
+++ src/components/NumbersInput.js
@@ -2,7 +2,8 @@ import React from "react"
 import styled from "styled-components"
 
 const NumbersInput = styled.input.attrs(() =>({
-    "type":"text"
+    "type":"text",
+    "id":"NumbersInput"
 }))``
 
 export default NumbersInput
--- src/components/Teclado.js
+++ src/components/Teclado.js
@@ -9,26 +9,118 @@ class Teclado extends React.Component{
 
         this.state = {
             numbersInput:"",
-            resultFinal:0
+            resultFinal:"",
+            factorOne:"",
+            factorTwo:"",
+            operation:""
         }
 
-        this.numbersInput = this.numbersInput.bind(this)
+        this.multiplicacao = this.multiplicacao.bind(this)
+        this.numbersInput  = this.numbersInput.bind(this)
+        this.subtracao     = this.subtracao.bind(this)
+        this.divisao       = this.divisao.bind(this)
+        this.apagar        = this.apagar.bind(this)
+        this.equal         = this.equal.bind(this)
+        this.soma          = this.soma.bind(this)
     }
 
     numbersInput(event) {
+        let value = event.target.innerText
+
         this.setState(state => {
-            return {numbersInput:state.numbersInput + event.target.innerText}
+            return {numbersInput:state.numbersInput + value}
         })
+
+        if(!this.state.operation) {
+            this.setState(state => {
+                return {factorOne:state.factorOne + value}
+            })
+        }else {
+            this.setState(state => {
+                return {factorTwo:state.factorTwo + value}
+            })
+        }
+    }
+
+    soma(event) {
+        let value = event.target.innerText
+
+        this.setState(state => {
+            return {numbersInput:state.numbersInput + value}
+        })
+
+        this.setState({operation:value})
+    }
+
+    multiplicacao(event) {
+        let value = event.target.innerText
+
+        this.setState(state => {
+            return {numbersInput:state.numbersInput + value}
+        })
+
+        this.setState({operation:value})
+    }
+
+    divisao(event) {
+        let value = event.target.innerText
+
+        this.setState(state => {
+            return {numbersInput:state.numbersInput + value}
+        })
+
+        this.setState({operation:value})
+    }
+
+    subtracao(event) {
+        let value = event.target.innerText
+
+        this.setState(state => {
+            return {numbersInput:state.numbersInput + value}
+        })
+
+        this.setState({operation:value})
+    }
+
+    porcentagem() {
+
+    }
+    
+    apagar() {
+        this.setState({numbersInput:""})
+    }
+
+    equal() {
+
+        if (this.state.operation == "+") {
+            let result = parseFloat(this.state.factorOne) + parseFloat(this.state.factorTwo)
+            this.setState({resultFinal:result})
+        }
+
+        if (this.state.operation == "-") {
+            let result = parseFloat(this.state.factorOne) - parseFloat(this.state.factorTwo)
+            this.setState({resultFinal:result})
+        }
+
+        if (this.state.operation == "*") {
+            let result = parseFloat(this.state.factorOne) * parseFloat(this.state.factorTwo)
+            this.setState({resultFinal:result})
+        }
+
+        if (this.state.operation == "/") {
+            let result = parseFloat(this.state.factorOne) / parseFloat(this.state.factorTwo)
+            this.setState({resultFinal:result})
+        }
     }
 
     render() {
         return (
             <>
-                <NumbersInput value={this.state.numbersInput}/>
-                <ResultFinal/>
+                <NumbersInput onChange={this.numbersInput} value={this.state.numbersInput}/>
+                <ResultFinal>{this.state.resultFinal}</ResultFinal>
 
                 <div className="teclado">
-                    <Button onClick={this.numbersInput}>9</Button>
+                    <Button onClick={this.numbersInput} value="9">9</Button>
                     <Button onClick={this.numbersInput}>8</Button>
                     <Button onClick={this.numbersInput}>7</Button>
                     <Button onClick={this.numbersInput}>6</Button>
@@ -39,14 +131,13 @@ class Teclado extends React.Component{
                     <Button onClick={this.numbersInput}>1</Button>
                     <Button onClick={this.numbersInput}>0</Button>
 
-                    <Button onClick={this.numbersInput}>C</Button>
-                    <Button onClick={this.numbersInput}>%</Button>
-                    <Button onClick={this.numbersInput}>/</Button>
-                    <Button onClick={this.numbersInput}>X</Button>
-                    <Button onClick={this.numbersInput}>-</Button>
-                    <Button onClick={this.numbersInput}>+</Button>
-                    <Button onClick={this.numbersInput}>=</Button>
-                    <Button onClick={this.numbersInput}>Apagar</Button>
+                    <Button onClick={this.apagar}>C</Button>
+                    <Button onClick={this.porcentagem}>%</Button>
+                    <Button onClick={this.divisao}>/</Button>
+                    <Button onClick={this.multiplicacao}>*</Button>
+                    <Button onClick={this.subtracao}>-</Button>
+                    <Button onClick={this.soma}>+</Button>
+                    <Button onClick={this.equal}>=</Button>
                     <Button onClick={this.numbersInput}>,</Button>
                 </div>
             </>
